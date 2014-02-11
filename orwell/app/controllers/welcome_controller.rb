5806900597907628f787c7bc351511635c177028
dchilot <dchilot@hotmail.com>
2014-02-11 23:17:29 +0000

    Cleaning.

--- orwell/app/controllers/welcome_controller.rb
+++
@@ -1,5 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    print "BATMAN goes ", params[:data]
-  end
-end
--- orwell/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Orwell::Application.config.session_store :cookie_store, key: '_orwell_session'
