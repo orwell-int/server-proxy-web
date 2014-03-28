137e4ba7ae54ac5bca0adb02505a660c138610a3
dchilot <dchilot@hotmail.com>
2014-03-28 21:17:36 +0000

    Use a configuration to store some hardcoded values.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,55 +1,79 @@
-$( document ).ready(function() 
-        {
-        //var ProtoBuf = require("protobufjs");
-        $( "a" ).click(function( event ) 
-            {
-            //event.preventDefault();
-            //$( this ).hide( "slow" );
-            alert( "Thanks for visiting!" );
-            });
-        $( document ).keydown(function(event) 
-            {
-            if( event.which == 37 )
-            {
-                event.preventDefault();
-                $("#textField").html("LEFT");
-                callServer("LEFT")
-            }
-            if( event.which == 38 )
-            {
-                event.preventDefault();
-                $("#textField").html("FORWARD");
-                callServer("FORWARD")
-            }
-            if( event.which == 39 )
-            {
-                event.preventDefault();
-                $("#textField").html("RIGHT");
-                callServer("RIGHT")
-            }
-            if( event.which == 40 )
-            {
-                event.preventDefault();
-                $("#textField").html("BACKWARD");
-                callServer("BACKWARD")
-            }
-            });
-        });
+$( document ).ready(
+	function()
+	{
+		//var ProtoBuf = require("protobufjs");
+		$( "a" ).click(
+			function( event )
+			{
+				//event.preventDefault();
+				//$( this ).hide( "slow" );
+				alert( "Thanks for visiting!" );
+			}
+		);
+		$( document ).keydown(
+			function(event)
+			{
+				if( event.which == 37 )
+				{
+					event.preventDefault();
+					$("#textField").html("LEFT");
+					callServer("LEFT")
+				}
+				if( event.which == 38 )
+				{
+					event.preventDefault();
+					$("#textField").html("FORWARD");
+					callServer("FORWARD")
+				}
+				if( event.which == 39 )
+				{
+					event.preventDefault();
+					$("#textField").html("RIGHT");
+					callServer("RIGHT")
+				}
+				if( event.which == 40 )
+				{
+					event.preventDefault();
+					$("#textField").html("BACKWARD");
+					callServer("BACKWARD")
+				}
+				if( event.which == 32 )
+				{
+					event.preventDefault();
+					$("#textField").html("FIRE2");
+					callServer("FIRE2")
+				}
+				if( event.which == 13 )
+				{
+					event.preventDefault();
+					$("#textField").html("FIRE1");
+					callServer("FIRE1")
+				}
+			}
+		);
+		$( document ).keyup(
+			function(event)
+			{
+				callServer("STOP")
+			}
+		);
+	}
+);
 
 
-function callServer(data) 
+function callServer(data)
 {
-    jQuery.ajax({
-        type: "POST",
-        url: "/welcome/index",
-        data: { "data": data },
-        dataType: "json",
-        success: function (data, status, jqXHR) {
-        // do something
-        },
+	jQuery.ajax({
+		type: "POST",
+		url: "/welcome/index",
+		data: { "data": data },
+		dataType: "json",
+		success: function (data, status, jqXHR) {
+			// do something
+		},
 
-        error: function (jqXHR, status) {
-        // error handler
-        }
-    });
+		error: function (jqXHR, status) {
+			// error handler
+		}
+	});
 }
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -15,13 +15,16 @@ end
 class WelcomeController < ApplicationController
   @@socket = nil
   @@context = nil
+  @@last_data = nil
   def initialize
     super
+
+    @videofeed=ENV['videofeed']
     if nil == @@socket
       @@context = ZMQ::Context.new
       @@socket = @@context.socket ZMQ::PUSH
       @@socket.setsockopt(ZMQ::LINGER, 0)
-      rc = @@socket.connect("tcp://192.168.1.37:9000")
+      rc = @@socket.connect(ENV['push_address'])
       error_check(rc)
       puts "Socket created"
     end
@@ -29,40 +32,50 @@ class WelcomeController < ApplicationController
     
   def index
     data = params[:data]
+    print "BATMAN went ", @@last_data, "\n"
+    if (data == @@last_data)
+      return
+    end
     print "BATMAN goes ", data, "\n"
+    @@last_data = data
+    #left = 0.001
+    #right = 0.001
     left = 0
     right = 0
     fire_weapon1 = false
     fire_weapon2 = false
+    factor = 0.5
     if ("LEFT" == data)
-      left = -1
-      right = 1
+      left = -1 * factor
+      right = 1 * factor
     elsif ("FORWARD" == data)
-      left = 1
-      right = 1
+      left = 1 * factor
+      right = 1 * factor
     elsif ("RIGHT" == data)
-      left = 1
-      right = -1
+      left = 1 * factor
+      right = -1 * factor
     elsif ("BACKWARD" == data)
-      left = -1
-      right = -1
+      left = -1 * factor
+      right = -1 * factor
+    elsif ("FIRE1" == data)
+      fire_weapon1 = true
+    elsif ("FIRE2" == data)
+      fire_weapon2 = true
     end
-    if left != 0
-      input = Orwell::Messages::Input.new(
-	:move => Orwell::Messages::Input::Move.new(
-	  :left => left,
-	  :right => right),
-	:fire => Orwell::Messages::Input::Fire.new(
-	  :weapon1 => fire_weapon1,
-	  :weapon2 => fire_weapon2))
-      print "input = '", input.to_json, "'\n"
-      bytes = input.to_s
-      zmq_message = "TANK_0 Input " + bytes
-      #print "bytes = '" + bytes.inspect + "'\n"
-      rc = @@socket.send_string(zmq_message)
-      if (not error_check(rc))
-	print "zmq_message = " + zmq_message.inspect + "'\n"
-      end
+    input = Orwell::Messages::Input.new(
+      :move => Orwell::Messages::Input::Move.new(
+	:left => left,
+	:right => right),
+      :fire => Orwell::Messages::Input::Fire.new(
+	:weapon1 => fire_weapon1,
+	:weapon2 => fire_weapon2))
+    print "input = '", input.to_json, "'\n"
+    bytes = input.to_s
+    zmq_message = "BananaOne Input " + bytes
+    #print "bytes = '" + bytes.inspect + "'\n"
+    rc = @@socket.send_string(zmq_message)
+    if (not error_check(rc))
+      print "zmq_message = " + zmq_message.inspect + "'\n"
     end
   end
 end
